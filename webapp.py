cf70f2aee48a5432b458f436bb51a34ea98f701d
Rek-ha <139787925+Rek-ha@users.noreply.github.com>
2024-05-01 10:12:08 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # Loading the saved model
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained model.sav', 'rb'))
 
 # Creating a function for prediction
 def diabetic_prediction(input_data):
